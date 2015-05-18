d50a1d35752dbc75d4a6ee1d2034d6c744f1d6f9
Vikram Raj Gopinathan <Vikram@Vikrams-MacBook-Pro.local>
2015-05-18 18:26:25 +0530

    Grunt file autoprefixer added to prefix bootstrap css

--- gruntfile.js
+++ gruntfile.js
@@ -1,52 +1,61 @@
+var cssFiles = {
+    'public/css/bootstrap.css': 'resources/assets/sass/bootstrap.scss',
+    'public/css/app.css': 'resources/assets/sass/app.scss'
+};
+
 module.exports = function (grunt) {
     grunt.initConfig({
-        // Load the package.json as pkg
-        pkg: grunt.file.readJSON('package.json'),
-
-        // Options for grunt-contrib-concat
         concat: {
             options: {
                 separator: ';\n'
             },
             dist: {
                 src: [
-                    './resources/assets/bower/jquery/dist/jquery.min.js',
-                    './resources/assets/bower/bootstrap-sass-official/assets/javascripts/bootstrap.min.js',
-                    './resources/assets/bower/handlebars/handlebars.min.js'
+                    'resources/assets/bower/jquery/dist/jquery.min.js',
+                    'resources/assets/bower/bootstrap-sass-official/assets/javascripts/bootstrap.min.js',
+                    'resources/assets/bower/angular/angular.min.js',
+                    'resources/assets/bower/angular-route/angular-route.min.js'
                 ],
                 dest: './public/js/vendor.js'
             }
         },
 
-        // Options for grunt-contrib-sass
         sass: {
             dev: {
                 options: {
                     style: 'expanded'
                 },
-                files: {
-                    './public/css/app.css': './resources/assets/sass/app.scss'
-                }
+                files: cssFiles
             },
             dist: {
                 options: {
                     style:'compressed'
                 },
-                files: {
-                    './public/css/app.css': './resources/assets/sass/app.scss'
-                }
+                files: cssFiles
             }
         },
 
-        // Options for grunt-phpspec
-        phpspec: {
+        postcss: {
             options: {
-                prefix: './vendor/bin/'
-            },
-            kivi: {
-                specs: 'app/Kivi'
+                map: true,
+                processors: [
+                    require('autoprefixer-core')({browsers: 'last 2 version'}).postcss
+                ],
+                dist: {
+                    src: 'public/css/bootstrap.css'
+                }
             }
-        },
+        }
+
+        // Options for grunt-phpspec
+        //phpspec: {
+        //    options: {
+        //        prefix: './vendor/bin/'
+        //    },
+        //    kivi: {
+        //        specs: 'app/Kivi'
+        //    }
+        //},
 
         // Options for grunt-notify
         //notify: {
@@ -59,27 +68,30 @@ module.exports = function (grunt) {
         //},
 
         // Options for grunt-contrib-watch
-        watch: {
-            sass: {
-                files: './resources/assets/sass/**/*.{scss,sass}',
-                tasks: ['sass:dist']
-            },
-            phpspec: {
-                files: [
-                    'app/Kivi/**/*.php',
-                    'app/spec/**/*.php'
-                ],
-                tasks: ['phpspec:kivi']
-            }
-        }
+        //watch: {
+        //    sass: {
+        //        files: './resources/assets/sass/**/*.{scss,sass}',
+        //        tasks: ['sass:dist']
+        //    },
+        //    phpspec: {
+        //        files: [
+        //            'app/Kivi/**/*.php',
+        //            'app/spec/**/*.php'
+        //        ],
+        //        tasks: ['phpspec:kivi']
+        //    }
+        //}
     });
 
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-phpspec');
-    //grunt.loadNpmTasks('grunt-notify');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.registerTask('styles', ['sass:dev', 'postcss']);
+    //grunt.loadNpmTasks('grunt-contrib-watch');
+    //grunt.loadNpmTasks('grunt-phpspec');
+    ////grunt.loadNpmTasks('grunt-notify');
 
-    grunt.registerTask('css', ['sass', 'watch:sass']);
-    grunt.registerTask('testing', ['phpspec:kivi', 'watch:phpspec']);
+    //grunt.registerTask('css', ['sass', 'watch:sass']);
+    //grunt.registerTask('testing', ['phpspec:kivi', 'watch:phpspec']);
+    grunt.registerTask('scripts', ['concat']);
 };
